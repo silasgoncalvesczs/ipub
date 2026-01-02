708d2f6a1078418c2b214cd8cf6381e3c31fd579
silasgoncalvesczs <silassilvaczs@hotmail.com>
2026-01-02 14:35:14 -0500

    test

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // 1. Efeito de Scroll na Navbar
 // Quando o usuário rola a página, a barra de navegação ganha uma cor de fundo.
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     const navbar = document.getElementById('navbar');
-    
+
     if (window.scrollY > 50) {
         navbar.classList.add('scrolled');
     } else {
